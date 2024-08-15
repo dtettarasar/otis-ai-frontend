226aa6203534715a8b39c95282f9b65fe3aef66d
Dylan Tettarasar <dtettarasar@gmail.com>
2024-08-15 10:27:59 +0000

    building the custom module getUserCreditBalance

--- src/custom_modules/getUserCreditBalance.js
+++ src/custom_modules/getUserCreditBalance.js
@@ -1,5 +1,16 @@
+import Cookies from 'js-cookie';
+
 export async function getUserCreditBalance() {
 
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const retrieveUserCreditBalanceUrl = backendUrl + 'front-api/user-credit-balance';
+
     console.log('init the custom module getUserCreditBalance()');
+    console.log(`backendUrl in the custom module getUserCreditBalance(): ${backendUrl}`);
+    console.log(`retrieveUserCreditBalanceUrl in the custom module getUserCreditBalance(): ${retrieveUserCreditBalanceUrl}`);
+
+    const accessToken = Cookies.get('accessToken');
+    console.log("accessToken in getUserCreditBalance() module: ");
+    console.log(accessToken);
 
 };
