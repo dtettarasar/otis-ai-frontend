35d0fc9770dd640929f08ebe876bc27dc5c2d5ae
Dylan Tettarasar <dtettarasar@gmail.com>
2024-09-16 17:18:45 +0000

    article viewer component: building the header

--- src/components/article_components/ArticleViewer.vue
+++ src/components/article_components/ArticleViewer.vue
@@ -4,6 +4,29 @@
 
     <p> {{ articleFromStore.title }}</p>
 
+    <div class="mt-2 p-5 bg-dark-subtle rounded" >
+
+        <h1>{{ articleFromStore.title }}</h1>
+
+        <p class="fs-5 date-text"><strong>creation date:</strong> {{ formattedDates.creationDate }}</p>
+        <p class="fs-5 date-text"><strong>last modification date:</strong> {{ formattedDates.lastModifDate }}</p>
+
+        <div>
+
+            <p class="fs-5">keywords:</p>
+
+            <div class="mb-2 d-flex justify-content-start flex-wrap">
+
+                <div class="badge m-1 p-1 bg-primary keyword-bdge d-flex flex-row" v-for="(keyword, index) in articleFromStore.keywordArr" :key="index">
+                    <p class="fs-6 m-1 align-self-center">{{keyword}}</p>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
     <div class="mt-4" >
 
         <div v-html="articleFromStore.content"></div>
@@ -46,6 +69,17 @@
 
             articleFromStore() {
                 return this.getArticleById(this.articleId);
+            },
+
+            formattedDates() {
+
+                return {
+
+                    creationDate: this.articleFromStore.creationDate ? new Date(this.articleFromStore.creationDate).toLocaleDateString() : '',
+                    lastModifDate: this.articleFromStore.lastModifDate ? new Date(this.articleFromStore.lastModifDate).toLocaleDateString() : '',
+
+                }
+
             },
 
         },
