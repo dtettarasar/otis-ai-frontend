44d1dddf57af321258a0582fd5be375b26659815
Dylan Tettarasar <dtettarasar@gmail.com>
2024-10-15 10:03:18 +0000

    keywordlist : get the keyword array from the store

--- src/components/article_components/KeywordsList.vue
+++ src/components/article_components/KeywordsList.vue
@@ -6,6 +6,9 @@
 
 <script>
 
+    import {mapGetters} from 'vuex';
+    import { toRaw } from 'vue';
+
     export default {
 
         name: 'KeywordsList',
@@ -21,9 +24,21 @@
 
             return {
 
-
+                keywordArr: [],
 
             }
+
+        },
+
+        computed: {
+
+            ...mapGetters({
+              getArticleById: 'getArticleById'
+            }),
+
+            articleFromStore() {
+                return this.getArticleById(this.articleId);
+            },
 
         },
 
@@ -33,6 +48,17 @@
 
                 console.log('article ID passed as props in KeywordList Component:');
                 console.log(this.articleId);
+
+                if (this.articleFromStore) {
+
+                    this.keywordArr = this.articleFromStore.keywordArr;
+                    console.log(toRaw(this.keywordArr));
+
+                } else {
+
+                    console.log('id not in the store');
+
+                }
 
             }
 
