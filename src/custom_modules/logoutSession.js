ce32d6ac0f2b9e39bbdd46caa828183958a78cce
Dylan Tettarasar <dtettarasar@gmail.com>
2024-11-08 15:16:32 +0000

    add a comment in the loggout session custom module

--- src/custom_modules/logoutSession.js
+++ src/custom_modules/logoutSession.js
@@ -1,5 +1,7 @@
 import Cookies from 'js-cookie';
 
+// todo: clear the local storage before loggout
+
 export function initLogout() {
     Cookies.remove('accessToken');
     Cookies.remove('refreshToken');
