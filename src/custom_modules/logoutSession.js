128e041cb79b63ca8b04c4b9ca6a140c6a20d34a
Dylan Tettarasar <dtettarasar@gmail.com>
2024-11-22 16:08:59 +0000

    loggout process: make sure we erase article list from local storage after user is logged out

--- src/custom_modules/logoutSession.js
+++ src/custom_modules/logoutSession.js
@@ -3,6 +3,7 @@ import Cookies from 'js-cookie';
 // todo: clear the local storage before loggout
 
 export function initLogout() {
+    localStorage.removeItem('articleDataList');
     Cookies.remove('accessToken');
     Cookies.remove('refreshToken');
     window.location.href = '/';
