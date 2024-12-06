8de744facaf53e0f59c218a9816a01e79cedee4a
Dylan Tettarasar <dtettarasar@gmail.com>
2024-12-06 17:31:27 +0000

    build the presenting content component

---
+++ src/components/general_content_components/PresentingContent.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+
+<template>
+
+    <div class="p-5">
+        
+        <h2>About Otis AI</h2>
+
+        <div>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam egestas sagittis diam, et pellentesque quam blandit et.
+                Aliquam volutpat dapibus faucibus. Pellentesque iaculis nisl ut tincidunt porttitor. Sed euismod arcu neque, sed fringilla sapien venenatis at.
+                In sollicitudin lacinia convallis. Nam ultrices dolor porta malesuada maximus. Aliquam laoreet eget mi et maximus. Curabitur pellentesque diam a turpis ultrices tristique.
+                Integer rutrum consectetur dolor quis egestas.
+            </p>
+
+            <p>
+                In eleifend sollicitudin arcu mattis vulputate. In aliquet nisl ac libero sagittis, vitae convallis neque hendrerit. Morbi aliquam ex euismod lorem ornare sodales. Pellentesque nec molestie erat, quis sodales erat.
+                Nam lacinia fringilla ligula sed commodo. Duis a efficitur dui, id rutrum mi. Curabitur maximus lectus et felis ultricies pulvinar. Nulla ac pellentesque diam, non vestibulum turpis. Nulla a ipsum a neque aliquet laoreet. 
+            </p>
+        </div>
+
+    </div>
+
+</template>
--- src/components/page_main_containers/HomeContent.vue
+++ src/components/page_main_containers/HomeContent.vue
@@ -1,11 +1,14 @@
 <script setup>
 
     import HomeBanner from '../general_content_components/HomeBanner.vue';
+    import PresentingContent from '../general_content_components/PresentingContent.vue';
 
 </script>
 
 <template>
 
     <HomeBanner></HomeBanner>
+
+    <PresentingContent></PresentingContent>
 
 </template>
