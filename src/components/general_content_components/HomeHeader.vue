c88ce557416928f8ad915e31e189026c45b5c2cd
Dylan Tettarasar <dtettarasar@gmail.com>
2024-12-06 17:00:17 +0000

    building the Header for the home

---
+++ src/components/general_content_components/HomeHeader.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+import { ref, onMounted } from 'vue'
+
+// lifecycle hooks
+onMounted(() => {
+  console.log(`the home header component is now mounted`);
+})
+
+</script>
+
+<template>
+
+    <h3>This is the home header component</h3>
+
+</template>
+
--- src/components/page_main_containers/HomeContent.vue
+++ src/components/page_main_containers/HomeContent.vue
@@ -1,43 +1,15 @@
+<script setup>
+
+    import HomeHeader from '../general_content_components/HomeHeader.vue';
+
+</script>
+
 <template>
 
-    
+    <HomeHeader></HomeHeader>    
+
     <div>
         <h2>This is the homepage content</h2>
     </div>
-    
 
 </template>
-
-<script>
-
-    export default {
-        name: 'HomeContent.vue',
-
-        data() {
-
-            return {
-
-            }
-
-        },
-
-        computed: {
-
-        },
-
-        mounted() {
-
-        },
-
-        beforeCreate() {
-        },
-
-        beforeMount() {
-        },
-
-        methods: {
-
-        }
-    }
-
-</script>
