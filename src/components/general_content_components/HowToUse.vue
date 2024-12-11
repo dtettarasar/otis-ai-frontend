57e18a57f35d66ab6d75fea6c6e2d8b8e358a27b
Dylan Tettarasar <dtettarasar@gmail.com>
2024-12-11 11:15:10 +0000

    building tab for the how to use section

--- src/components/general_content_components/HowToUse.vue
+++ src/components/general_content_components/HowToUse.vue
@@ -23,9 +23,9 @@
             </p>
         </div>
 
+        <!--
         <div class="d-flex flex-row justify-content-evenly flex-wrap">
 
-            <!-- Boucle sur les items -->
             <IconAndTextVertical v-for="(item, index) in howToUseData" 
                 :key="index"
                 :iconClass="item.iconClass"
@@ -35,7 +35,21 @@
             ></IconAndTextVertical>
 
         </div>
-        
+        -->
+
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <li class="nav-item" role="presentation">
+                <button class="nav-link active" id="describe-tab" data-bs-toggle="tab" data-bs-target="#describe" type="button" role="tab" aria-controls="describe" aria-selected="true">Describe</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#generate" type="button" role="tab" aria-controls="generate" aria-selected="false">Generate</button>
+            </li>
+        </ul>
+
+        <div class="tab-content" id="myTabContent">
+            <div class="tab-pane fade show active" id="describe" role="tabpanel" aria-labelledby="home-tab">Describe content</div>
+            <div class="tab-pane fade" id="generate" role="tabpanel" aria-labelledby="profile-tab">Generate content</div>
+        </div>
 
     </div>
 
